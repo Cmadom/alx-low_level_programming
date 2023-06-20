e6c02c97581974f2220dfa0fd2da42a4c2872261
Cmadom <cmadom@st.ug.edu.gh>
2023-06-20 03:16:15 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    int v;
+
+    v = SUM(98, 1024);
+    printf("%d\n", v);
+    return (0);
+}
